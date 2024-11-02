0064a89feabe362eb83540fbbaaf76e5c09ee2ac
Muhammed Midlaj PC <midlajpc531@gmail.com>
2024-11-02 14:02:06 +0530

    age calculator

--- prompt.js
+++ prompt.js
@@ -1,11 +1,12 @@
+const input = require("prompt-sync")();
 const agefinder = () => {
-  const input = require("prompt-sync")();
   const year = input("eneter your dob:-");
   const date = new Date();
   const currentyear = date.getFullYear();
   if (year < currentyear) {
     const age = 2024 - year;
     console.log(age);
+    
   } else {
     console.log("enter a valid year");
   }
