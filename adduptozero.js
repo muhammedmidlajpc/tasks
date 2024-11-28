8d835edc56019e03297121393b23cd170cb7a559
Midlaj PC <midlajpc531@gmail.com>
2024-11-28 13:12:30 +0530

    addUpToZero

--- adduptozero.js
+++ adduptozero.js
@@ -1,8 +1,16 @@
-const zero=(n)=>{
-    let arr=[]
-    let idx=0
-    for (let i = 0; i < Math.floor(n/2); i++) {
-       
-        
-    }
-}
+const zero = (n) => {
+  let arr = [];
+  let idx = 0;
+  const m = (n / 2) | 0;
+  for (let i = 1; i <= m; i++) {
+    arr[idx] = i;
+    idx++;
+    arr[idx] = -i;
+    idx++
+  }
+  if (n % 2 !== 0) {
+    arr[idx] = 0;
+  }
+  return arr;
+};
+console.log(zero(6));
